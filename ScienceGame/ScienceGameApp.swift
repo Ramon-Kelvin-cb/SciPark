22eee454410a05383b9bfc37e00f9f2c9ddb0190
Ramon <=>
2023-04-26 13:10:42 -0400

    Initial Commit

---
+++ ScienceGame/ScienceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScienceGameApp.swift
+//  ScienceGame
+//
+//  Created by Ramon Kelvin on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ScienceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
